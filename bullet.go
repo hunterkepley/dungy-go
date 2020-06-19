0c2013239bf5f954c089e82075ebaf8b8059d3ca
Hunter Kepley <kepley.l.hunter@gmail.com>
2020-06-19 03:08:27 -0400

    Created bullet and gun files

---
+++ bullet.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/hajimehoshi/ebiten"
+
+type bullet struct {
+	image *ebiten.Image
+}
---
+++ gun.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/hajimehoshi/ebiten"
+)
+
+type gun struct {
+	image *ebiten.Image
+}

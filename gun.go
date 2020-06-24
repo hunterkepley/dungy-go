168f82264adba65d866b87f1a451d7a902afc475
Hunter Kepley <kepley.l.hunter@gmail.com>
2020-06-23 21:46:35 -0400

    Gun is fine except doesn't move with player

--- gun.go
+++ gun.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"math"
 
@@ -10,9 +11,9 @@ import (
 // Gun is the player's gun
 type Gun struct {
 	position Vec2f
-	offset   Vec2f
 
 	rotation float64
+	flipped  bool
 
 	sprite Sprite
 	image  *ebiten.Image
@@ -22,6 +23,11 @@ func (g *Gun) render(screen *ebiten.Image) {
 	op := &ebiten.DrawImageOptions{}
 	// Translate center of image to 0, 0 before rotating
 	op.GeoM.Translate(0-float64(g.sprite.size.x)/2, 0-float64(g.sprite.size.y)/2)
+	if g.flipped {
+		op.GeoM.Scale(1, -1)
+	} else {
+		op.GeoM.Scale(1, 1)
+	}
 	op.GeoM.Rotate(g.rotation)
 	op.GeoM.Translate(g.position.x, g.position.y)
 	op.Filter = ebiten.FilterNearest // Maybe fix rotation grossness?
@@ -42,12 +48,39 @@ func (g *Gun) update(playerPosition Vec2f) {
 	cursorPosition.x, cursorPosition.y = ebiten.CursorPosition()
 
 	// Placement offset [circle]
-	g.offset = newVec2f(0, 0)
-	radius := 20.
+	radius := 15.
 	angle := math.Atan2(playerPosition.y-float64(cursorPosition.y), playerPosition.x-float64(cursorPosition.x))
 
-	g.position.x = playerPosition.x - radius*math.Cos(angle) // Starting position x
-	g.position.y = playerPosition.y - radius*math.Sin(angle) // Starting position y
+	// Flip gun image
+	if angle < Pi/2 && angle > Pi/-2 {
+		if !g.flipped {
+			g.flipped = true
+			if angle > 0 {
+				angle -= Pi / 6
+			} else {
+				angle += Pi / 6
+			}
+		}
+	} else {
+		if g.flipped {
+			g.flipped = false
+			if angle > 0 {
+				angle += Pi / 6
+			} else {
+				angle -= Pi / 6
+			}
+		}
+	}
+
+	// This if statement only allows it to move on the sides of the body
+	if !(angle > Pi/3 && angle < Pi/3+Pi/3) && !(angle < Pi/-3 && angle > Pi/-3-Pi/3) {
+		g.position.x = playerPosition.x - radius*math.Cos(angle) // Starting position x
+		g.position.y = playerPosition.y - radius*math.Sin(angle) // Starting position y
+
+		fmt.Println(angle)
+	}
+
+	// TODO: Make the gun go to other side once it flips
 
 	// Make always face the mouse
 	g.rotation = angle + 135

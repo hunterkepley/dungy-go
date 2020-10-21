58b0e8a7fc0798403565b9fa48285ca46c6d6bd2
hunterkepley <kepley.l.hunter@gmail.com>
2020-10-21 17:12:59 -0400

    Basic phases work now
    -- Added test tile
    -- Added phase handler code to map.go

--- phases.go
+++ phases.go
@@ -1,15 +1,20 @@
 package main
 
-import "math/rand"
+import (
+	"image"
+	"math/rand"
+
+	"github.com/hajimehoshi/ebiten"
+)
 
 // PhaseChunk is a chunk of a "map" to phase in randomly
 type PhaseChunk struct {
-	tiles    []Tile
+	tiles    []PhaseTile
 	enemies  []Enemy
 	mapNodes []string
 }
 
-func createPhaseChunk(tiles []Tile, enemies []Enemy, mapNodes []string) PhaseChunk {
+func createPhaseChunk(tiles []PhaseTile, enemies []Enemy, mapNodes []string) PhaseChunk {
 	return PhaseChunk{
 		tiles:    tiles,
 		enemies:  enemies,
@@ -17,17 +22,16 @@ func createPhaseChunk(tiles []Tile, enemies []Enemy, mapNodes []string) PhaseChu
 	}
 }
 
-/*
- * This function handles the phase changes, or, the chunks of new maps that replace the current one
- *
- * Lots to do
- */
-func (p *Phases) phaseHandler() {
-	if p.timer > 0 {
-		p.timer--
-	} else {
-		p.timer = p.timerMax
-	}
+// PhaseTile is a stripped down Tile struct for easier usage with the phase chunk struct
+type PhaseTile struct {
+	tileType  TileType
+	image     *ebiten.Image
+	imageRect image.Rectangle
+	empty     bool // Whether or not this tile actually changes
+}
+
+func createPhaseTile(tileType TileType, image *ebiten.Image, imageRect image.Rectangle, empty bool) PhaseTile {
+	return PhaseTile{tileType, image, imageRect, empty}
 }
 
 // Phases is a struct containing all the phases in the game and some controls for them
@@ -42,8 +46,8 @@ func initPhases() Phases {
 	p := Phases{
 		chunks: []PhaseChunk{},
 
-		timer:    500,
-		timerMax: 500,
+		timer:    50,
+		timerMax: 50,
 	}
 
 	p.makePhases()
@@ -51,19 +55,73 @@ func initPhases() Phases {
 	return p
 }
 
+/*
+ * This function handles the phase changes, or, the chunks of new maps that replace the current one
+ *
+ * Lots to do
+ */
+func (p *Phases) phaseHandler() {
+	if p.timer > 0 {
+		p.timer--
+	} else {
+		p.phase()
+		p.timer = p.timerMax
+	}
+}
+
 func (p *Phases) makePhases() {
+	testTilePosition := image.Rect(102, 0, 118, 17)
 	p.addPhaseChunk(
-		[]Tile{},
+		[]PhaseTile{
+			createPhaseTile(SmallTile, itileSpritesheet, testTilePosition, true),
+			createPhaseTile(SmallTile, itileSpritesheet, testTilePosition, true),
+			createPhaseTile(SmallTile, itileSpritesheet, testTilePosition, true),
+			createPhaseTile(SmallTile, itileSpritesheet, testTilePosition, true),
+			createPhaseTile(SmallTile, itileSpritesheet, testTilePosition, true),
+		},
 		[]Enemy{},
-		[]string{},
+		[]string{
+			"xxxx",
+			"xxxx",
+			"xxxx",
+			"xxxx",
+		},
 	)
 }
 
 func (p *Phases) getRandomPhase() PhaseChunk {
-	return p.chunks[rand.Intn(len(p.chunks))]
+	if len(p.chunks) > 0 {
+		return p.chunks[rand.Intn(len(p.chunks))]
+	}
+	return PhaseChunk{}
 }
 
-func (p *Phases) addPhaseChunk(tiles []Tile, enemies []Enemy, mapNodes []string) {
+func (p *Phases) addPhaseChunk(tiles []PhaseTile, enemies []Enemy, mapNodes []string) {
 	c := createPhaseChunk(tiles, enemies, mapNodes)
 	p.chunks = append(p.chunks, c)
+}
+
+func (p *Phases) phase() {
+	// Get random chunk
+	chosenChunk := p.getRandomPhase()
+
+	// Choose random tile
+	chosenTile, index := getRandomTile(gameReference)
+
+	if len(chosenChunk.tiles) > 0 {
+		for i := 0; i < len(chosenChunk.mapNodes); i++ {
+			for j := 0; j < len(chosenChunk.mapNodes[i]); j++ {
+				if index.x+i < len(gameReference.tiles) &&
+					index.y+j < len(gameReference.tiles[i]) &&
+					gameReference.tiles[index.x+i][index.y+j].tileType == SmallTile &&
+					i+j < len(chosenChunk.tiles) {
+					gameReference.tiles[index.x+i][index.y+j].imageRect = chosenChunk.tiles[i+j].imageRect
+				}
+
+			}
+		}
+	}
+
+	_ = chosenTile
+	_ = chosenChunk
 }
